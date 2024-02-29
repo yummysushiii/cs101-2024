16d92e6146e2f433eac892e2bd4ce1c6d02e03d5
yummysushiii <89094866+yummysushiii@users.noreply.github.com>
2024-02-29 15:27:40 +0800

    Add files via upload

---
+++ main1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+	char a[] = "AABBBCCCCddd";
+	int j = 1;
+	for(int i = 0; i < sizeof(a)-1; i++){
+		if(a[i] == a[i+1]){
+			j = j+1;
+			
+			continue;
+		}
+		printf("%c", a[i]);
+		printf("%d", j);
+		j=1;
+	}	
+	return 0;
+} 
