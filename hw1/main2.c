e28f10f2e71b98554ac569187e9391e3a37cc4fa
yummysushiii <89094866+yummysushiii@users.noreply.github.com>
2024-02-29 15:39:10 +0800

    Update main2.c

--- hw1/main2.c
+++ hw1/main2.c
@@ -4,7 +4,7 @@ int main(){
 	char a[] = "A4B1C3f3";
 	for(int i = 0; i < sizeof(a)-1; i++){
 		if(i%2!=0){
-			for(j = 0; j < a[i]-48; j++){
+			for(int j = 0; j < a[i]-48; j++){
 				printf("%c", a[i-1]);
 			}
 		}
